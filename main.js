e120e758c2699f1b49359a40589b6b7539cdba59
Apollo <sostenesapollo25@gmail.com>
2022-10-26 08:31:30 -0300

    feat: server and print

--- main.js
+++ main.js
@@ -1,6 +1,88 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Tray, Menu } = require("electron");
 const path = require("path");
+
+
+const { PosPrinter } = require("electron-pos-printer");
+
+function date() {
+  const x = new Date();
+
+  const y = "0" + x.getHours();
+  const z = "0" + x.getMinutes();
+  const s = "0" + x.getSeconds();
+  const h = "0" + x.getDate();
+  const ano = x.getFullYear().toString().substr(-2);
+  const ms = x.getMonth();
+  const meses = [
+    "Jan",
+    "Fev",
+    "Mar",
+    "Abr",
+    "Mai",
+    "Jun",
+    "Jul",
+    "Ago",
+    "Set",
+    "Out",
+    "Nov",
+    "Dez",
+  ];
+
+  return (
+    y.substr(-2) +
+    ":" +
+    z.substr(-2) +
+    ":" +
+    " -  " +
+    h.substr(-2) +
+    "/" +
+    meses[ms]
+  );
+}
+
+function print(orders) {
+  // console.log(orders);
+  
+  let rows = []
+
+  for(let order of orders) {
+    console.log(order);
+    rows = [
+      ...rows,
+      ...order.map(s=>({
+        type: 'text',                                       
+        value: typeof s === 'string' ? s : s.text,
+        style: `text-align:left;`,
+        css: typeof s === 'string' ? 
+          {"font-weight": "300", "font-size": "12px","font-family": "Arial" } : 
+          {"font-weight": "bold", "font-size": "12px","font-family": "Arial" }
+      })),
+      {
+        type: 'text',                                       
+        value: '_______________________________',
+        style: `text-align:left;`,
+        css: {"font-weight": "300", "font-size": "12px","font-family": "Arial" }
+      }
+   ]
+  }
+
+  console.log('print', rows);
+
+  PosPrinter.print(rows, {
+    preview: false, // Preview in window or print
+    width: '200px', //  width of content body
+    margin: "0 0 0 0", // margin of content body
+    copies: 1, // Number of copies to print
+    printerName: 'POS', // printerName: string, check it at webContent.getPrinters()
+    timeOutPerLine: 400,
+    silent: true,
+  })
+    .then(() => {})
+    .catch((error) => {
+      console.error(error);
+    });
+}
 
 function createWindow() {
   // Create the browser window.
@@ -11,6 +93,7 @@ function createWindow() {
       preload: path.join(__dirname, "preload.js"),
       nodeIntegration: true,     // ATENÇÃO AQUI - attention here - that's why your node_module works or not
     },
+    show: false
   });
 
   // and load the index.html of the app.
@@ -25,6 +108,24 @@ function createWindow() {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow();
+  
+  tray = new Tray(path.join(__dirname, '/assets/icon.png'))
+  tray.setToolTip('Printer Server')
+  tray.setContextMenu(Menu.buildFromTemplate([
+    {
+      label: 'Test print', click: function () {
+        
+        console.log(print);
+        print()
+      }
+    },
+    {
+      label: 'Quit', click: function () {
+        isQuiting = true;
+        app.quit();
+      }
+    }
+  ]));
 
   app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
@@ -33,12 +134,30 @@ app.whenReady().then(() => {
   });
 });
 
-// Quit when all windows are closed, except on macOS. There, it's common
+// Quit when all windows    are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on("window-all-closed", function () {
   if (process.platform !== "darwin") app.quit();
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+
+const express = require('express')
+var server = express();
+var http = require('http').createServer(server);
+
+server.use(express.json()); 
+server.use(express.urlencoded()); 
+
+server.post('/print', (req, res)=>{
+    try {        
+        const { orders } = req.body
+        print(orders)
+        res.send('ok')
+    }catch(e) {
+      console.log(e);
+        res.status(400).send({error: e.stack})
+    }
+})
+
+server.listen(3001, ()=>console.log('listening http at port 3001'))
--- renderer.js
+++ renderer.js
@@ -1,83 +1,6 @@
 let { remote } = require("electron");
-// console.log(process.versions.electron);
-
 const { PosPrinter } = remote.require("electron-pos-printer");
-// const {PosPrinter} = require("electron-pos-printer"); //dont work in production (??)
-
 const path = require("path");
-
-let webContents = remote.getCurrentWebContents();
-let printers = webContents.getPrinters(); //list the printers
-console.log(printers);
-
-printers.map((item, index) => {
-  //write in the screen the printers for choose
-  document.getElementById("list_printers").innerHTML +=
-    ' <input type="radio" id="printer_' +
-    index +
-    '" name="printer" value="' +
-    item.name +
-    '"><label for="printer_' +
-    index +
-    '">' +
-    item.name +
-    "</label><br>";
-});
-
-
-const data = [
-  {
-    type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table
-    value: "||---",
-    style: `text-align:left;`,
-    css: { "font-size": "12px" },
-  },
-  {
-    type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table
-    value: "HEADER",
-    style: `text-align:center;`,
-    css: { "font-weight": "700", "font-size": "18px" },
-  },
-  {
-    type: "image",
-    path: path.join(__dirname, "assets/img_test.png"), // file path
-    position: "center", // position of image: 'left' | 'center' | 'right'
-    width: "auto", // width of image in px; default: auto
-    height: "60px", // width of image in px; default: 50 or '50px'
-  },
-  {
-    type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table'
-    value:
-      "Lorem ipsum<br><br> . , ; : ( ) - + = ! # % \" ' <br><br> ã Ã ç Ç $ & @ ê Ê í Í<br><br> 0 1 2 3 4 5 6 7 8 9 <br>a b c d e f g h i j k l m n o p q r s t u v w x y z<br>A B C D E F G H I J K L M N O P Q R S T U V W X Y Z<br><br><hr><br>elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation \n ullamco \n laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum<br>",
-
-    css: {
-      "font-size": "12px",
-      "font-family": "sans-serif",
-      "text-align": "center",
-    },
-  },
-  {
-    type: "barCode", // Do you think the result is ugly? Me too. Try use an image instead...
-    value: "HB4587896",
-    height: 12,
-    width: 1,
-    displayValue: true, // Display value below barcode
-    fontsize: 8,
-  },
-  {
-    type: "qrCode",
-    value: "https://github.com/fssonca",
-    height: 80,
-    width: 80,
-    style: "margin-left:50px",
-  },
-  {
-    type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table
-    value: "---||",
-    style: `text-align:right;`,
-    css: { "font-size": "12px" },
-  },
-];
 
 function date() {
   const x = new Date();
@@ -90,17 +13,17 @@ function date() {
   const ms = x.getMonth();
   const meses = [
     "Jan",
-    "Feb",
+    "Fev",
     "Mar",
-    "Apr",
-    "May",
-    "June",
-    "July",
-    "Aug",
-    "Sept",
-    "Oct",
+    "Abr",
+    "Mai",
+    "Jun",
+    "Jul",
+    "Ago",
+    "Set",
+    "Out",
     "Nov",
-    "Dec",
+    "Dez",
   ];
 
   return (
@@ -108,7 +31,6 @@ function date() {
     ":" +
     z.substr(-2) +
     ":" +
-    s.substr(-2) +
     " -  " +
     h.substr(-2) +
     "/" +
@@ -117,56 +39,80 @@ function date() {
 }
 
 function print() {
-  let printerName;
-  let widthPage;
-
-  var p = document.getElementsByName("printer");
-  var w = document.getElementsByName("width");
-
-  for (var i = 0, length = p.length; i < length; i++) {
-    if (p[i].checked) {
-      printerName = p[i].value;
-
-      break;
+  PosPrinter.print([
+    {
+      type: "image",
+      path: path.join(__dirname, "assets/apollo.png"), // file path
+      position: "center", // position of image: 'left' | 'center' | 'right'
+      width: "auto", // width of image in px; default: auto
+      height: "30px", // width of image in px; default: 50 or '50px'
+    },
+    // {
+    //   type: "text", // 'text' | 'barCode' | 'qrCode' | 'image' | 'table
+    //   value: "APOLLO GÀS",
+    //   style: `text-align:center;font-family:Arial`,
+    //   css: { "font-weight": "300", "font-size": "18px" },
+    // },
+    {
+      type: 'table',
+      // style the table
+      style: 'border: 1px solid #ddd',
+      // list of the columns to be rendered in the table header
+      tableHeader: ['Animal', 'Age'],
+      // multi dimensional array depicting the rows and columns of the table body
+      tableBody: [
+          ['Cat', 2],
+          ['Dog', 4],
+          ['Horse', 12],
+          ['Pig', 4],
+      ],
+      // list of columns to be rendered in the table footer
+      tableFooter: ['Animal', 'Age'],
+      // custom style for the table header
+      tableHeaderStyle: 'background-color: #000; color: white;',
+      // custom style for the table body
+      tableBodyStyle: 'border: 0.5px solid #ddd',
+      // custom style for the table footer
+      tableFooterStyle: 'background-color: #000; color: white;',
+    },
+    {
+      type: 'table',
+      style: 'border: 1px solid #ddd',             // style the table
+      // list of the columns to be rendered in the table header
+      tableHeader: [{type: 'text', value: 'Animal'}, {type: 'image', path: path.join(__dirname, 'icons/animal.png')}],
+      // multi dimensional array depicting the rows and columns of the table body
+      tableBody: [
+          [{type: 'text', value: 'Cat'}, {type: 'image', path: './animals/cat.jpg'}],
+          [{type: 'text', value: 'Dog'}, {type: 'image', path: './animals/dog.jpg'}],
+          [{type: 'text', value: 'Horse'}, {type: 'image', path: './animals/horse.jpg'}],
+          [{type: 'text', value: 'Pig'}, {type: 'image', path: './animals/pig.jpg'}],
+      ],
+      // list of columns to be rendered in the table footer
+      tableFooter: [{type: 'text', value: 'Animal'}, 'Image'],
+      // custom style for the table header
+      tableHeaderStyle: 'background-color: #000; color: white;',
+      // custom style for the table body
+      tableBodyStyle: 'border: 0.5px solid #ddd',
+      // custom style for the table footer
+      tableFooterStyle: 'background-color: #000; color: white;',
+   },
+    {
+      type: "text",
+      value: "" + date(),
+      style: `text-align:center;`,
+      css: { "font-size": "12px", "font-family": "sans-serif" },
     }
-  }
-
-  for (var i = 0, length = w.length; i < length; i++) {
-    if (w[i].checked) {
-      widthPage = w[i].value;
-
-      break;
-    }
-  }
-
-  console.log(printerName, widthPage);
-
-  const options = {
+  ], {
     preview: false, // Preview in window or print
-    width: widthPage, //  width of content body
+    width: '200px', //  width of content body
     margin: "0 0 0 0", // margin of content body
     copies: 1, // Number of copies to print
-    printerName: printerName, // printerName: string, check it at webContent.getPrinters()
+    printerName: 'POS', // printerName: string, check it at webContent.getPrinters()
     timeOutPerLine: 400,
     silent: true,
-  };
-
-  const now = {
-    type: "text",
-    value: "" + date(),
-    style: `text-align:center;`,
-    css: { "font-size": "12px", "font-family": "sans-serif" },
-  };
-
-  const d = [...data, now];
-
-  if (printerName && widthPage) {
-    PosPrinter.print(d, options)
-      .then(() => {})
-      .catch((error) => {
-        console.error(error);
-      });
-  } else {
-    alert("Select the printer and the width");
-  }
+  })
+    .then(() => {})
+    .catch((error) => {
+      console.error(error);
+    });
 }
